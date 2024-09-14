89d35a3662ed537c4ae5e3df6cfac5fcef80b104
just_very_queer <vatsalvaibhav.2013@gmail.com>
2024-09-14 17:14:15 +0530

    Initial Commit

---
+++ MustardTests/MustardTests.swift
@@ -0,0 +1,16 @@
+//
+//  MustardTests.swift
+//  MustardTests
+//
+//  Created by VAIBHAV SRIVASTAVA on 14/09/24.
+//
+
+import Testing
+
+struct MustardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
